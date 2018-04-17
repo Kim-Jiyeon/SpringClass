ee1a566f8427d10a2f14aaeddc8843f457361c4c
Kim-Jiyeon <gjy00727@e-mirim.hs.kr>
2018-04-17 19:02:56 +0900

    'SpringPractice'

---
+++ src/com/Hello.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Hello {
+    public void printMessage(String message){
+
+        System.out.println("received message : " + message);
+    }
+}
---
+++ src/com/Main.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Main {
+    public static void main(String[] args){
+        //Hello Class 생성
+        Hello hello = new Hello();
+        hello.printMessage("hello world2");
+    }
+}
